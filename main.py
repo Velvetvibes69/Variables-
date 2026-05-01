81af27b155cf666772c842ad04e301022fb1b484
Velvetvibes69 <amayasunil11@gmail.com>
2026-05-01 19:48:33 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#Creating different variables
+x = 5
+y = "Amaya"
+print(x)
+print(y)
+
+Codingal = "Codingal"
+print(Codingal)
+
+#User Input
+name = input ("Enter your name: ")
+
+print("\nHello", name, "\nWelcome to Codingal")
